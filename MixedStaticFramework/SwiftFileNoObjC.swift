fcd765b584122b31f13ec1dc4c0ee43b47ebeb89
Bryan Boyko <bryan@maxprebid.com>
2018-02-14 14:57:58 -0800

    add swift file that doesnt use any objc

---
+++ MixedStaticFramework/SwiftFileNoObjC.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftFileNoObjC.swift
+//  MixedStaticFramework
+//
+//  Created by Bryan Boyko on 2/14/18.
+//  Copyright © 2018 Bryan Boyko. All rights reserved.
+//
+
+import Foundation
+
+class SwiftFileNoObjC {
+    
+    func describe() {
+        print("This is a swift file from the static framework that does not import any objc")
+    }
+}
